5c0a56ccd39a2393cafd936a6541b1a7e92fe820
Le Trung Son <sonmartin2003@gmail.com>
2025-09-10 17:31:35 +0700

    Updated eslint configs

--- eslint-prettier-config-sample-for-react-project/eslint.config.js
+++ eslint-prettier-config-sample-for-react-project/eslint.config.js
@@ -1,52 +1,47 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default [
-  { ignores: ["dist", "node_modules"] },
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      'prettier' // Should installed eslint-config-prettier
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: "module"
+        sourceType: 'module'
       }
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh
-    },
     rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true }
-      ],
-
-      "react/prop-types": 0,
-      "react/display-name": 0,
-      "no-console": 1,
-      "no-lonely-if": 1,
-      "no-trailing-spaces": 1,
-      "no-multi-spaces": 1,
-      "no-multiple-empty-lines": 1,
-      "space-before-blocks": ["error", "always"],
-      "object-curly-spacing": [1, "always"],
-      indent: ["warn", 2],
-      semi: [1, "never"],
-      "array-bracket-spacing": 1,
-      "linebreak-style": 0,
-      "no-unexpected-multiline": "warn",
-      "keyword-spacing": 1,
-      "comma-dangle": 1,
-      "comma-spacing": 1,
-      "arrow-spacing": 1
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/prop-types': 0,
+      'react/display-name': 0,
+      'no-lonely-if': 1,
+      'no-trailing-spaces': 1,
+      'no-multi-spaces': 1,
+      'no-multiple-empty-lines': 1,
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': [1, 'always'],
+      indent: ['warn', 2],
+      semi: [1, 'never'],
+      'array-bracket-spacing': 1,
+      'linebreak-style': 0,
+      'no-unexpected-multiline': 'warn',
+      'keyword-spacing': 1,
+      'comma-dangle': 1,
+      'comma-spacing': 1,
+      'arrow-spacing': 1
     }
   }
-];
+])
